466b60b1d9bdb71c3b91a9298f27dcfb4d92c58c
prithvihv <hvprithvi09@gmail.com>
2021-02-09 09:37:35 +0530

    fix(sql): faster table, to handle inserts

--- src/sqlite/comment.sql
+++ src/sqlite/comment.sql
@@ -3,16 +3,16 @@ PRAGMA recursive_triggers = ON;
 PRAGMA max_page_count = 4294967292;
 
 -- https://kimsereylam.com/sqlite/2020/03/06/full-text-search-with-sqlite.html
-CREATE TABLE IF NOT EXISTS comment (id INTEGER PRIMARY KEY,
-                                    reddit_id TEXT UNIQUE NOT NULL,
+CREATE UNLOGGED TABLE IF NOT EXISTS comment (id INTEGER PRIMARY KEY,
+                                    reddit_id TEXT,
                                     author TEXT,
                                     subreddit TEXT,
                                     body TEXT,
-                                    score INTEGER NOT NULL,
-                                    created_utc INTEGER NOT NULL,
+                                    score INTEGER ,
+                                    created_utc INTEGER ,
                                     retrieved_on INTEGER,
-                                    parent_id TEXT NOT NULL,
-                                    parent_is_post BOOLEAN NOT NULL);
+                                    parent_id TEXT ,
+                                    parent_is_post BOOLEAN ) with (autovacuum_enabled=false);
 
 CREATE INDEX IF NOT EXISTS idx_parent_id ON comment (parent_id);
 CREATE INDEX IF NOT EXISTS idx_author ON comment (author);
